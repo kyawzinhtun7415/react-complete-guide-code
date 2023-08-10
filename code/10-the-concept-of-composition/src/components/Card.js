c67cedc3232abe66938cf20c241c187c71426244
Kyaw Zin Tun @ Wong Wun Kwint <72453322+kyawzinhtun7415@users.noreply.github.com>
2023-08-10 10:41:03 +0800

    Complete 10-the-concept-of-composition ( Understand how to use props.children )

--- code/10-the-concept-of-composition/src/components/Card.js
+++ code/10-the-concept-of-composition/src/components/Card.js
@@ -1,9 +1,9 @@
 import './Card.css';
 
 function Card(props) {
-  const classes = 'card ' + props.className;
-  
-  return <div className={classes}>{props.children}</div>;
+  const classes = 'card ' + props.className; // props.className will let you apply class to the Card component (which html works out of the box)
+  console.log(classes);
+  return <div className={classes}>{props.children}</div>; // to enable a wrapper component, add {props.children} OR IT WILL NOT WORK!!
 }
 
 export default Card;
